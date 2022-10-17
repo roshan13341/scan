32fa3e847a359c2548d30ce9faca248a84e4b173
roshan13341 <51996163+roshan13341@users.noreply.github.com>
2022-10-17 14:50:27 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MrzReaderComponent } from './mrz-reader/mrz-reader.component';
+import { MrzScannerComponent } from './mrz-scanner/mrz-scanner.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { NgxMrzSdkModule } from 'ngx-mrz-sdk';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MrzReaderComponent,
+    MrzScannerComponent,
+    ProductListComponent,
+    TopBarComponent,
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    RouterModule,
+    
+    NgxMrzSdkModule.forRoot({ 
+      licenseKey: "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==", 
+      dceResourcePath: "assets/dynamsoft-camera-enhancer", 
+      dlrResourcePath: "assets/dynamsoft-label-recognizer"}),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
